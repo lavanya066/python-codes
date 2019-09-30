319b962b00f968a69baf16836a5c9199db704d40
lavanya066 <51395856+lavanya066@users.noreply.github.com>
2019-09-30 09:43:07 +0530

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,4 +1,4 @@
-a=[]
+a=[]#this is to find the max sum of non adjecent elements
 l=int(input('enter the limit'))
 for i in range(l):
     b=int(input('enter'))
@@ -8,15 +8,15 @@ v=[]
 k=[]
 
     
-for i in range(len(a)):
+for i in range(len(a)):#seperating even and odd part of the elements
     if(i%2!=0):
         v.append(a[i])
     if(i%2==0):
         k.append(a[i])
 p=len(k)
 g=len(v)
-if ((len(a))%2==0 and k[p-1]<a[len(a)-1]):
-    k.remove(k[p-1])
+if ((len(a))%2==0 and k[p-1]<a[len(a)-1]):#length of matrix is to be even else if we replace the number will not be adjecent
+    k.remove(k[p-1])#remove and replace as the first and last place of the matrix will not be adjecent
     k.append(a[len(a)-1])
 if (v[0]<a[0]):
     v.remove(v[0])
